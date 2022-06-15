d12c646d13ee51963c55b9b0b96f9c79b1042f77
shailender1911 <kumarjase43@gmail.com>
2022-06-15 18:06:21 +0530

    second commit to check rebase

--- src/main/java/com/javatechie/crud/example/SpringBootCrudExample2Application.java
+++ src/main/java/com/javatechie/crud/example/SpringBootCrudExample2Application.java
@@ -2,10 +2,11 @@ package com.javatechie.crud.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class SpringBootCrudExample2Application {
 
+
+// shailender kumargit
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootCrudExample2Application.class, args);
 	}
